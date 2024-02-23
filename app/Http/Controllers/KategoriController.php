e5e504b30ba400e0efc54ba0cbd4b61cf6485159
Milanurlaela <milanurlaela0717@gmail.com>
2024-02-23 13:24:58 +0700

    ubah

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Kategori;
-use App\Models\Kategoribukurelasi;
 class KategoriController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $kategori = Kategori::all();
-        return view ('kategori.kategori', ['kategori'=>$kategori]);
+        return view ('kategori.kategori',['kategori'=>$kategori]);
     }
     public function create(){
         return view ('kategori.create_kategori');
@@ -21,12 +21,32 @@ class KategoriController extends Controller
         Kategori::create([
             'nama_kategori'=>$request->nama_kategori
         ]);
-        return redirect ('/kategori')->with('succes', 'Kategori berhasil ditambahkan!');
+        return redirect ('/kategori')->with('success', 'Kategori berhasil ditambahkan!');
     }
-    public function hapus($id)
+     public function hapus ($id)
     {
-    $kategori = Kategori::find($id);
-    $kategori->delete();
-    return redirect('/kategori');
+        $kategori = Kategori::find($id);
+        $kategori->delete();
+        return redirect ('/kategori');
     }
+    public function edit($id)
+{
+    $kategori = Kategori::findOrFail($id);
+    return view('kategori.edit', ['kategori'=>$kategori]);
+}
+public function update(Request $request, $id)
+{
+    $request->validate([
+        'nama_kategori'=>'required',
+        
+
+    ]);
+    Kategori::find($id)->update([
+        'nama_kategori' => $request->nama_kategori,
+        
+    ]);
+   
+
+    return redirect('/kategori');
+}
 }
