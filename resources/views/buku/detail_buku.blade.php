e312e66cdc1ec7666cea25887a5f12e36a236d3f
Milanurlaela <milanurlaela0717@gmail.com>
2024-03-14 12:38:00 +0700

    perubahan

--- resources/views/buku/detail_buku.blade.php
+++ resources/views/buku/detail_buku.blade.php
@@ -8,36 +8,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-md-5"> <!-- Adjust the column size based on your preference -->
-                <div class="card mb-5">
-                <img src="{{ asset('storage/'.$buku->foto) }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <table class="table table stripped">
+<br>
+<div class="container-fluid">
+    <div class="row ">
+        <div class=" d-flex justify-content-center"> <!-- Adjust the column size based on your preference -->
+            <div class="card-img">
+                <img  src="{{ asset('storage/' . $buku->foto) }}" class="card-img-top" alt="...">
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table stripped">
+                        <tr>
+                            <th>Judul Buku: </th>
+                            <td>{{$buku->judul}}</td>
+                        </tr>
+                        <tr>
+                            <th>Penulis: </th>
+                            <td>{{$buku->penulis}}</td>
+                        </tr>
+                        <tr>
+                            <th>Penerbit: </th>
+                            <td>{{$buku->penerbit}}</td>
+                        </tr>
+                        <tr>
+                            <th>Tahun Terbit: </th>
+                            <td>{{$buku->tahun_terbit}}</td>
                             <tr>
-                                <th>Judul Buku: </th>
-                                <td>{{$buku->judul}}</td>
+                                <th>Sinopsis: </th>
+                                <td>{{$buku->sinopsis}}</td>
                             </tr>
-                            <tr>
-                                <th>Penulis: </th>
-                                <td>{{$buku->penulis}}</td>
-                            </tr>
-                            <tr>
-                                <th>Penerbit: </th>
-                                <td>{{$buku->penerbit}}</td>
-                            </tr>
-                            <tr>
-                                <th>Tahun Terbit: </th>
-                                <td>{{$buku->tahun_terbit}}</td>
-                                <tr>
-                                    <th>Sinopsis: </th>
-                                    <td>{{$buku->sinopsis}}</td>
-                                </tr>
-                            </tr>
-                        </table>
-                    </div>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
